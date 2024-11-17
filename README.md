e21dce561323856462561b8d8d5690c99b9630ab
Sylvan Smit <sylvan@hey.com>
2024-11-17 13:16:30 +0100

    initial commit
    Signed-off-by: Sylvan Smit <sylvan@hey.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+# Wald
+
+A demo app, which combines elm charts and a rest app
+
+# source of data
+https://www.ncdc.noaa.gov/cdo-web/webservices/v2#gettingStarted
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {body:?}");
+    Ok(())
+}

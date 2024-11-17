0cd08ebf227276b6cded4b037342ccd6f33794b7
Sylvan Smit <sylvan@hey.com>
2024-11-17 15:23:35 +0100

    List out optional cities
    Signed-off-by: Sylvan Smit <sylvan@hey.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use reqwest::Url;
+//{"mindate":"1876-01-01","maxdate":"2024-11-14","name":"Berlin, GM","datacoverage":1,"id":"CITY:GM000001"}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = std::env::var("API_KEY")?;
     let base_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/locations";
-    let client = reqwest::Client::new();
-    let res = client.get(base_url).header("token", api_key).send().await?;
+    let url = Url::parse_with_params(
+        base_url,
+        &[
+            ("locationcategoryid", "CITY"),
+            ("sortfield", "name"),
+            ("sortorder", "asc"),
+            ("limit", "1000"),
+        ],
+    )?;
 
-    println!("res = {res:?}");
+    let client = reqwest::Client::new();
+    let res = client.get(url).header("token", api_key).send().await?;
+
+    let text = res.text().await?;
+
+    //let json: serde_json::Value = serde_json::from_str(&text)?;
+    //let json_pretty = serde_json::to_string_pretty(&json)?;
+    println!("res = {text}");
     Ok(())
 }

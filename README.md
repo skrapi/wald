aaa1f198c618063b134824431f77baa20689b7e9
Sylvan Smit <sylvan@hey.com>
2024-11-17 14:35:30 +0100

    Basic successful access
    Signed-off-by: Sylvan Smit <sylvan@hey.com>

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ A demo app, which combines elm charts and a rest app
 
 # source of data
 https://www.ncdc.noaa.gov/cdo-web/webservices/v2#gettingStarted
+
+# Running
+```sh
+API_KEY=<api_key> cargo r
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let body = reqwest::get("https://www.rust-lang.org")
-        .await?
-        .text()
-        .await?;
+    let api_key = std::env::var("API_KEY")?;
+    let base_url = "https://www.ncei.noaa.gov/cdo-web/api/v2/locations";
+    let client = reqwest::Client::new();
+    let res = client.get(base_url).header("token", api_key).send().await?;
 
-    println!("body = {body:?}");
+    println!("res = {res:?}");
     Ok(())
 }
